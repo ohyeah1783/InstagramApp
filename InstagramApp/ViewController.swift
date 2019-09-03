3c8ed251034ab965cf496de3252dc13991da0511
ohyeah1783 <kawaishinta@gmail.com>
2019-09-03 10:54:50 +0900

    Initial Commit

---
+++ InstagramApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramApp
+//
+//  Created by Shintaro Kawai on 2019/09/03.
+//  Copyright © 2019 shintaro.kawai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
